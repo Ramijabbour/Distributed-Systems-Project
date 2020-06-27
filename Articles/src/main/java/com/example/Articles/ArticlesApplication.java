0a9b9a526322b760a147342f3487704c78320fb2
Rami Jabbour <ramijab.565@gmail.com>
2020-06-27 12:31:11 -0400

    add eureka client

--- Articles/src/main/java/com/example/Articles/ArticlesApplication.java
+++ Articles/src/main/java/com/example/Articles/ArticlesApplication.java
@@ -2,8 +2,10 @@ package com.example.Articles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ArticlesApplication {
 
 	public static void main(String[] args) {
