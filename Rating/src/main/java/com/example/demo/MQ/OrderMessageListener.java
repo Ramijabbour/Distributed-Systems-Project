1c53173d82391754de9f42615c754b0a08c0d4f3
khalilalzought <49902609+khalilalzought@users.noreply.github.com>
2020-06-29 20:14:48 +0300

    mq rating

--- Rating/src/main/java/com/example/demo/MQ/OrderMessageListener.java
+++ Rating/src/main/java/com/example/demo/MQ/OrderMessageListener.java
@@ -1,8 +1,10 @@
 package com.example.demo.MQ;
+import com.example.Service.RatingService;
 import com.example.demo.MQ.ModelToRecv.ArticleID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +13,16 @@ public class OrderMessageListener {
 
     static final Logger logger = LoggerFactory.getLogger(OrderMessageListener.class);
 
+    @Autowired
+    RatingService ratingService ;
+    
     @RabbitListener(queues = "ArticleIdToRating")
     public void processOrder(ArticleID articleID) {
-        System.out.println("Article ID recieved with info : " + articleID.id);
+    	
+ 	   System.out.println("request to delete Rate of article #"+articleID.getId());	   
+        this.ratingService.deleteArticleRating(articleID.getId());
+ 	   System.out.println("delete operation done !");
+
         logger.info("Order Received: " + articleID);
     }
 }
