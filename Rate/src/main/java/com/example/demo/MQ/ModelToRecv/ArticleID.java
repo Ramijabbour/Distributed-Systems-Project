9c4e32d36d8a2759f34325bf50f824bdef6ad138
khalilalzought <49902609+khalilalzought@users.noreply.github.com>
2020-07-02 20:14:12 +0300

    .

--- Rate/src/main/java/com/example/demo/MQ/ModelToRecv/ArticleID.java
+++ Rate/src/main/java/com/example/demo/MQ/ModelToRecv/ArticleID.java
@@ -3,6 +3,9 @@ package com.example.demo.MQ.ModelToRecv;
 import java.io.Serializable;
 
 public class ArticleID implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
     public int id ;
     public ArticleID() {
     }
