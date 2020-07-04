6e7da3b36304206668a11470bab28f178ed5d805
Mohammed_Altujjar <mohammed_.1996@live.com>
2020-07-04 21:06:36 +0300

    merge #2

--- Articles/src/main/java/com/example/Articles/ArticleController.java
+++ Articles/src/main/java/com/example/Articles/ArticleController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,23 +28,6 @@ public class ArticleController {
 	public String Ping() {
 		return zone;
 	}
-
-	@Value("${eureka.instance.metadataMap.zone}")
-	private String zone;
-	@RequestMapping(method = RequestMethod.GET , value = "/ping")
-	public String Ping() {
-		return zone;
-	}
-
-	
-	@Value("${eureka.instance.metadataMap.zone}")
-	private String zone ; 
-	
-	@RequestMapping("/ping")
-	public String ping() {
-		return zone ; 
-	}
-	
 	
 	@RequestMapping(method = RequestMethod.GET , value = "/all")
 	public ArticleList allArticle()
--- WebApp/src/main/java/com/example/WebApp/WebApp/AppController.java
+++ WebApp/src/main/java/com/example/WebApp/WebApp/AppController.java
@@ -20,7 +20,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
